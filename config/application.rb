74c8de7a97da2d891c1d0e8a794198aa04778c13
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-02-29 04:03:43 +0530

    Day 36 Add token genrater in test environment  for reset password and confirm account

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module PhotoApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    Rails.application.routes.default_url_options[:host] = 'http://127.0.0.1:3000'
+
   end
 end
