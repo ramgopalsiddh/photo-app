1959930a5b8fab90f5c2cd23c3465fc1ce611b6c
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-03 20:42:49 +0530

    Day 39 Add Image Upload (model,db & controllers)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_223414) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_03_124141) do
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
+
   create_table "payments", force: :cascade do |t|
     t.string "email"
     t.string "token"
@@ -35,4 +44,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_223414) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "users"
 end
